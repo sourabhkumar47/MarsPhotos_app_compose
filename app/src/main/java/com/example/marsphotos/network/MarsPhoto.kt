a0e2f9a024b2c8695536bf93191e724562f7c354
Sourabh Kumar <20010001030@gateway.edu.in>
2023-06-22 23:55:34 +0530

    created data class

--- app/src/main/java/com/example/marsphotos/network/MarsPhoto.kt
+++ app/src/main/java/com/example/marsphotos/network/MarsPhoto.kt
@@ -1,3 +1,15 @@
 package com.example.marsphotos.network
 
-data class MarsPhoto()
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class MarsPhoto(
+    val id: String,
+
+    //To use variable names in your data class that
+    // differ from the key names in the JSON response,
+    // use the @SerialName annotation
+    @SerialName(value = "img_src")
+    val imgSrc: String,
+)
