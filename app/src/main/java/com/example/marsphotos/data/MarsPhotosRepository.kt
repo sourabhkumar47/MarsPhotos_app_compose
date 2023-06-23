e1849228418b412b834d74b5ea223b0dd1ced9e1
Sourabh Kumar <20010001030@gateway.edu.in>
2023-06-23 22:46:26 +0530

    created data repo

--- app/src/main/java/com/example/marsphotos/data/MarsPhotosRepository.kt
+++ app/src/main/java/com/example/marsphotos/data/MarsPhotosRepository.kt
@@ -1,7 +1,15 @@
 package com.example.marsphotos.data
 
+import com.example.marsphotos.network.MarsApi
 import com.example.marsphotos.network.MarsPhoto
 
 interface MarsPhotosRepository {
+    //fetch list of MarsPhoto from API
     suspend fun getMarsPhotos(): List<MarsPhoto>
+}
+
+class NetworkMarsPhotoRepository() : MarsPhotosRepository{
+    override suspend fun getMarsPhotos(): List<MarsPhoto> {
+        return MarsApi.retrofitService.getPhotos()
+    }
 }
