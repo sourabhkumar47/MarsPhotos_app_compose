5128c64885a7a44bdfceffe5268aa803f663e347
Sourabh Kumar <20010001030@gateway.edu.in>
2023-06-25 00:17:40 +0530

    fixed error

--- app/src/main/java/com/example/marsphotos/MarsPhotosApplication.kt
+++ app/src/main/java/com/example/marsphotos/MarsPhotosApplication.kt
@@ -4,9 +4,8 @@ import android.app.Application
 import com.example.marsphotos.data.AppContainer
 import com.example.marsphotos.data.DefaultAppContainer
 
-class MarsPhotosApplication: Application() {
+class MarsPhotosApplication : Application() {
     lateinit var container: AppContainer
-
     override fun onCreate() {
         super.onCreate()
         container = DefaultAppContainer()
--- app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
+++ app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
@@ -26,6 +26,8 @@ import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.marsphotos.MarsPhotosApplication
 import com.example.marsphotos.data.MarsPhotosRepository
+import com.example.marsphotos.data.NetworkMarsPhotoRepository
+import com.example.marsphotos.network.MarsApiService
 import com.example.marsphotos.network.MarsPhoto
 import kotlinx.coroutines.launch
 import retrofit2.HttpException
@@ -35,12 +37,12 @@ import java.io.IOException
  * UI state for the Home screen
  */
 sealed interface MarsUiState {
-    data class Success(val photos: List<MarsPhoto>) : MarsUiState
+    data class Success(val photos: String) : MarsUiState
     object Error : MarsUiState
     object Loading : MarsUiState
 }
 
-class MarsViewModel(private val marsPhotosRepository: MarsPhotosRepository) : ViewModel() {
+class MarsViewModel(private val marsPhotosRepository: MarsPhotosRepository) : ViewModel(){
     /** The mutable State that stores the status of the most recent request */
     var marsUiState: MarsUiState by mutableStateOf(MarsUiState.Loading)
         private set
@@ -58,9 +60,9 @@ class MarsViewModel(private val marsPhotosRepository: MarsPhotosRepository) : Vi
      */
     fun getMarsPhotos() {
         viewModelScope.launch {
-            marsUiState = MarsUiState.Loading
             marsUiState = try {
-                MarsUiState.Success(marsPhotosRepository.getMarsPhotos())
+                val listResult = marsPhotosRepository.getMarsPhotos()
+                MarsUiState.Success("Success .${listResult.size} Mars photos retrieved")
             } catch (e: IOException) {
                 MarsUiState.Error
             } catch (e: HttpException) {
