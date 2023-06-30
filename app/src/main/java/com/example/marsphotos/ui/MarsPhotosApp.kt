c725919f4c84f012c08778eaaccfe134a232c436
Sourabh Kumar <20010001030@gateway.edu.in>
2023-07-01 02:48:33 +0530

    modified top app bar

--- app/src/main/java/com/example/marsphotos/ui/MarsPhotosApp.kt
+++ app/src/main/java/com/example/marsphotos/ui/MarsPhotosApp.kt
@@ -16,31 +16,50 @@
 
 package com.example.marsphotos.ui
 
+import androidx.compose.foundation.MutatePriority
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.outlined.LocationOn
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.marsphotos.R
 import com.example.marsphotos.ui.screens.HomeScreen
 import com.example.marsphotos.ui.screens.MarsViewModel
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MarsPhotosApp() {
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
     Scaffold(
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = { MarsTopAppBar(scrollBehavior = scrollBehavior) }
+
     ) {
         Surface(
             modifier = Modifier
@@ -51,7 +70,7 @@ fun MarsPhotosApp() {
                 viewModel(factory = MarsViewModel.Factory)
             HomeScreen(
                 marsUiState = marsViewModel.marsUiState,
-                retryAction  = marsViewModel::getMarsPhotos
+                retryAction = marsViewModel::getMarsPhotos
             )
         }
     }
@@ -59,15 +78,37 @@ fun MarsPhotosApp() {
 
 @Composable
 fun MarsTopAppBar(scrollBehavior: TopAppBarScrollBehavior, modifier: Modifier = Modifier) {
-    CenterAlignedTopAppBar(
+//    CenterAlignedTopAppBar(
+//        scrollBehavior = scrollBehavior,
+//        title = {
+//            Text(
+//                text = stringResource(R.string.app_name),
+//                style = MaterialTheme.typography.headlineSmall,
+//            )
+//        },
+//        modifier = modifier
+//    )
+
+    var isClicked by remember { mutableStateOf(false) }
+
+
+    TopAppBar(
         scrollBehavior = scrollBehavior,
         title = {
             Text(
-                text = stringResource(R.string.app_name),
-                style = MaterialTheme.typography.headlineSmall,
+                stringResource(R.string.app_name),
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
             )
         },
-        modifier = modifier
+        actions = {
+            IconButton(onClick = {  isClicked = !isClicked }) {
+                Icon(
+                    imageVector = if(isClicked) Icons.Filled.LocationOn else Icons.Outlined.LocationOn,
+                    contentDescription = "Localized description"
+                )
+            }
+        }
     )
 }
 
